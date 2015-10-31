de831f16dd919e186900048e1815cfc7a6699279
abhialla <abhilasha1203@gmail.com>
2015-11-01 00:06:55 +0530

    deleting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
-     def create
-     end
-     def edit
-     end
-     def delete
-     end
+    
 end
